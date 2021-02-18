882488dbd980ea1ef76ed45821191e6031f3863b
Guilherme Fazam <glrm1991@gmail.com>
2021-02-18 17:20:41 -0300

    404 PageError

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import { Title } from '../styles/pages/Home';
+
+export default function NotFound() {
+    return (
+        <main>
+            <section>
+                <Title>Pagina não existe</Title>
+            </section>
+        </main>
+    );
+}
